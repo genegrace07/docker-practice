3c3d1add9f99d4947617007e7c7508c0b1d6f569
mark anthony dador <dador.markanthony@gmail.com>
2026-01-18 21:05:43 +0800

    modify update_infor ongoing, for docker

--- flask_api/app.py
+++ flask_api/app.py
@@ -17,17 +17,33 @@ def view_data():
     except json.JSONDecodeError:
         abort(400,description='json not found')
 
-@app.route('/addinfo',methods=['POST','GET'])
+@app.route('/addinfo',methods=['POST'])
 def add_info():
     try:
         json_data()
-        new_data = {'id':1,'name':'luffy','email':'luffy@gmail.com'}
+        new_data = [{'id':1,'name':'luffy','email':'luffy@gmail.com'},
+                    {'id':2,'name':'zoro','email':'zoro@gmail.com'},
+                    {'id':3,'name':'sanji','email':'sanji@gmail.com'}]
         with open(datas,'w') as f:
             json.dump(new_data,f,indent=4)
         return jsonify({'message':'Successfully added'})
 
     except FileNotFoundError:
         abort(404,description='json not found')
+@app.route('/updateinfo',methods=['POST','GET'])
+def update_info():
+    try:
+        data = json_data()
+        new_data = {'id':6,'name':'zoro','email':'luffy@gmail.com'}
+        if new_data['id'] in data['id']:
+            return jsonify({'message': 'already exist'})
+        data.append(new_data)
+        with open(datas,'w') as f:
+            json.dump(data,f,indent=4)
+        return jsonify({'message': 'Successfully added'})
+
+    except FileNotFoundError:
+        abort(404,description='json not found')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=5000)
